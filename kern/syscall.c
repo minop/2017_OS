d935f13a1c2c8c4f4ec96e22468043df5f831939
Jakub Kovar <jakub@kovar.co.uk>
2017-12-07 05:54:55 +0100

    lab5: vypracovanie ulohy 7 z MIT stranky

--- kern/syscall.c
+++ kern/syscall.c
@@ -146,7 +146,19 @@ sys_env_set_trapframe(envid_t envid, struct Trapframe *tf)
 	// LAB 5: Your code here.
 	// Remember to check whether the user has supplied us with a good
 	// address!
-	panic("sys_env_set_trapframe not implemented");
+
+	// overime adresu
+	user_mem_assert(curenv, tf, sizeof(struct Trapframe), PTE_U);
+
+	// zisame prostredie
+	struct Env* e;
+	int r;
+
+	r = envid2env(envid, &e, 1);
+	if(r < 0) return r;
+
+	e->env_tf = *tf;
+	return 0;
 }
 
 // Set the page fault upcall for 'envid' by modifying the corresponding struct
@@ -468,6 +480,9 @@ syscall(uint32_t syscallno, uint32_t a1, uint32_t a2, uint32_t a3, uint32_t a4, 
 		return sys_ipc_try_send((envid_t)a1, (uint32_t)a2, (void *)a3, (unsigned)a4);
 	case SYS_ipc_recv:
 		return sys_ipc_recv((void *)a1);
+	// lab5
+	case SYS_env_set_trapframe:
+		return sys_env_set_trapframe((envid_t)a1, (struct Trapframe*)a2);
 	default:
 		return -E_INVAL;
 	}
