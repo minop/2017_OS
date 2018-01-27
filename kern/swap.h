e5bdff28e0db146a49dd85e804b0237dea3d621c
Jakub Kovar <jakub@kovar.co.uk>
2018-01-27 13:46:09 +0100

    Zavedenie kern/swap.h a kern/swap.c

---
+++ kern/swap.h
@@ -0,0 +1,9 @@
+#ifndef JOS_KERN_SWAP_H
+#define JOS_KERN_SWAP_H
+#ifndef JOS_KERNEL
+# error "This is a JOS kernel header; user programs should not #include it"
+#endif
+
+#include <inc/swap.h>
+
+#endif /* !JOS_KERN_SWAP_H */
